dc07d418d3b901953628f13cc5e4725ef7112133
viveloper <ucsik1234@gmail.com>
2022-08-09 21:30:50 +0900

    feat: trello 라우트 추가

---
+++ src/routes/trello/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Trello</title>
+	<meta name="description" content="Trello App" />
+</svelte:head>
+
+<div class="todos">
+	<h1>Trello</h1>
+</div>
